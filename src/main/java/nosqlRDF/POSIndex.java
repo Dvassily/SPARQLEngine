40849fe750d6bcdb4ba1d5cf134599212e43d3c6
Luke <luke.lo-seen@etu.umontpellier.fr>
2020-11-08 18:36:51 +0100

    Benchmark_5indexs

---
+++ src/main/java/nosqlRDF/POSIndex.java
@@ -0,0 +1,24 @@
+package nosqlRDF;
+
+import java.util.Set;
+import java.math.BigInteger;
+
+public class POSIndex extends AbstractHexastoreIndex {
+    public POSIndex(Dictionary dictionary) {
+	super(dictionary);
+    }
+    
+    public Set<RDFTriple> findObjectSubject(String predicate) {
+	return findYZ(predicate);
+    }
+
+    public Set<RDFTriple> findSubject(String predicate, String object) {
+	return findZ(predicate, object);
+    }
+
+    @Override
+    protected BigInteger composeKey(BigInteger subjectKey, BigInteger predicateKey, BigInteger objectKey) {
+	return composeKeyInternal(predicateKey, objectKey,subjectKey);
+    }
+
+}
